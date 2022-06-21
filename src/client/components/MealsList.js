b9a2fc8010ceb1528b0c87be471366642dc53351
Hussein Khaleefah <husseinkhaleefah@Husseins-MacBook-Pro.local>
2022-06-21 20:25:25 +0200

    U

--- src/client/components/MealsList.js
+++ src/client/components/MealsList.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import mealsContext from "./MealsContext";
 import { useMeals } from "./UseMeals";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapPin } from '@fortawesome/free-solid-svg-icons'
+import { faMapPin, faCoins } from '@fortawesome/free-solid-svg-icons'
 
 import "./Style.css";
 
@@ -17,8 +17,8 @@ export default function MealsList() {
           <li className="recipeMealsContainer" key={aMeal.idMeals}>
             <Link to={`/meal/${aMeal.idMeals}`}>{aMeal.title}</Link>
             <span>{aMeal.description}</span>
-            <span><FontAwesomeIcon icon={faMapPin} /> {aMeal.location}</span>
-            <span>{aMeal.price} DKK </span>
+            <span><FontAwesomeIcon icon={faMapPin}/> {aMeal.location}</span>
+            <span> <FontAwesomeIcon icon={faCoins}/> {aMeal.price} DKK </span>
           </li>
         );
       });
